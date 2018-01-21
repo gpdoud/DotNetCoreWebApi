25d984f6e4b339a537ff7abbc6cd45ef8aa286a1
Greg doud <gpdoud@gmail.com>
2018-01-21 09:33:22 -0500

    Added EntityFrameworkCore to the project. Connected to a SQL database and successfully added a record then read the record with get.

--- DotNetCoreWebApi/Startup.cs
+++ DotNetCoreWebApi/Startup.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreWebApi.models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DotNetCoreWebApi
 {
     public class Startup
     {
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration) {
+			Configuration = configuration;
+		}
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddDbContext<TodoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
 			services.AddMvc();
         }
 
---
+++ DotNetCoreWebApi/models/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWebApi.models
+{
+    public class TodoContext : DbContext
+    {
+		// do this for easy dependency injection
+		public TodoContext(DbContextOptions<TodoContext> options) : base(options) {
+
+		}
+
+		public DbSet<Todo> Todos { get; set; }
+    }
+}
